323a2420146f7730e41c0a7b6c9d82f75500e3a1
Sergio Delgado Quintero <sdelquin@gmail.com>
2019-10-20 12:36:35 +0100

    Change database backend to PostgreSQL

--- base/settings.py
+++ base/settings.py
@@ -87,12 +87,10 @@ WSGI_APPLICATION = "base.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": config('DATABASE_NAME'),
         "USER": config('DATABASE_USER'),
         "PASSWORD": config('DATABASE_PASSWORD'),
-        "HOST": "localhost",
-        "OPTIONS": {"charset": "utf8mb4"},
     }
 }
 
