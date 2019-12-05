6f0142f8f3885d1d22b96af39366361352c365c8
Sergio Delgado Quintero <sdelquin@gmail.com>
2019-12-05 18:39:29 +0000

    Fix entrypoint for video names

--- base/settings.py
+++ base/settings.py
@@ -191,7 +191,7 @@ RQ = Queue(connection=REDIS)
 
 GOOGLE_API = config('GOOGLE_API')
 
-NAMES_URL = "http://names.sdelquin.me/random/"
+NAMES_URL = "https://names.sdelquin.me/people/random/"
 
 LOGIN_URL = "/login/"
 
