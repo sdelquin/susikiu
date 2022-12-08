d0bd511de427aedd968a6b95c3488d6e8e349568
Sergio Delgado Quintero <sdelquin@gmail.com>
2022-12-08 12:27:06 +0000

    Fix production setup

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-source ~/.virtualenvs/susikiu/bin/activate
+source ~/.pyenv/versions/susikiu/bin/activate
 cd $(dirname $0)
 exec gunicorn -c gunicorn.conf.py base.wsgi:application
--- run_rq.sh
+++ run_rq.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Script to launch RQ (Redis Queue) service
 
-source ~/.virtualenvs/susikiu/bin/activate
+source ~/.pyenv/versions/susikiu/bin/activate
 cd $(dirname $0)
 exec rq worker
